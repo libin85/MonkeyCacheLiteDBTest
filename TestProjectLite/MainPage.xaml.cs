028b9160548b9db4f988086522f5abf53bd17a6f
Libin Joseph <libin.joseph@outlook.com>
2019-01-14 15:19:26 +1100

    Initial commit

---
+++ TestProjectLite/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonkeyCache.LiteDB;
+using Xamarin.Forms;
+
+namespace TestProjectLite
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            var abc = Barrel.Current.Get<string>(key: "TestKey");
+            Barrel.Current.EmptyAll();
+            var abc1 = Barrel.Current.Get<string>(key: "TestKey");
+            InitializeComponent();
+        }
+    }
+}
